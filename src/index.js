6fc2364ae24d845cf7b3b34c2c3a60739d52410c
Kelvin Liang <kelvin9877@gmail.com>
2020-01-20 12:02:29 +0800

    inital first step 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import ship from './assets/js/ship';
 import './assets/stylesheet/style.sass';
 
+const Controller = (() => {
 
+
+  const init = () => {
+
+  }
+
+  return {
+    init,
+  }
+})();
+
+Controller.ini();
 
 export const start = () => {
   return 'Hello';
